ce4636996f1be61c36d9c36f18cad96b72ac3d47
sebasvega95 <sebas060495@gmail.com>
2017-06-03 13:38:20 -0500

    regularized logistic regression

--- lib/estimator.py
+++ lib/estimator.py
@@ -4,22 +4,26 @@ import tensorflow as tf
 
 class BinaryLogisticRegression:
     def __init__(self, num_features):
-        self.weights = tf.Variable(tf.random_normal([num_features, 1]))
+        self.weights = tf.Variable(tf.random_normal([num_features, 1], stddev=0.1))
         self.bias = tf.Variable(tf.random_normal([1]))
         self.sess = tf.Session()
 
     def close(self):
         self.sess.close()
 
-    def fit(self, train_X, train_y, learning_rate=3, training_epochs=200, batch_size=300, report_step=float('inf')):
+    def fit(self, train_X, train_y, learning_rate=3, training_epochs=200,
+            batch_size=300, report_step=float('inf'), lamda=0.0):
         m, n = train_X.shape
         X = tf.placeholder(tf.float32, [None, n])
         y = tf.placeholder(tf.float32, [None, 1])
 
         logits = tf.matmul(X, self.weights) + self.bias
         cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=y))
-        optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+        regularization = 0.5 * lamda * tf.reduce_sum(self.weights * self.weights) / batch_size
+        total_cost = cost + regularization
+        optimizer = tf.train.AdamOptimizer(learning_rate).minimize(total_cost)
 
+        min_cost = float('inf')
         self.sess.run(tf.global_variables_initializer())
         self.sess.run(tf.local_variables_initializer())
         for epoch in range(training_epochs):
@@ -28,8 +32,14 @@ class BinaryLogisticRegression:
             batch_y = train_y[indices, :]
             feed = {X: batch_X, y: batch_y}
             _, epoch_cost = self.sess.run([optimizer, cost], feed_dict=feed)
+            if epoch_cost < min_cost:
+                min_cost = epoch_cost
+                min_weights = self.sess.run(self.weights)
+                min_bias = self.sess.run(self.bias)
             if (epoch + 1) % report_step == 0:
                 print('Epoch {:04d}, cost = {}'.format(epoch + 1, epoch_cost))
+        self.sess.run(tf.assign(self.weights, min_weights))
+        self.sess.run(tf.assign(self.bias, min_bias))
 
     def predict(self, test_X):
         X = tf.constant(test_X, dtype=tf.float32)
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 
 from lib.estimator import BinaryLogisticRegression
 from lib.smote_data import load_data
 
 
 def main():
+    print('Loading data')
     X, y = load_data('smote_data.npz', 'data.csv')
+
     scaler = StandardScaler()
     X = scaler.fit_transform(X)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=0)
@@ -15,8 +17,9 @@ def main():
     num_features = X.shape[1]
     clf = BinaryLogisticRegression(num_features)
 
+    print('Training')
     y_train.shape = (y_train.size, 1)
-    clf.fit(X_train, y_train, training_epochs=1200, report_step=200)
+    clf.fit(X_train, y_train, training_epochs=10000, report_step=500)
 
     predictions = clf.predict(X_test)
     accuracy = accuracy_score(y_test, predictions)
